29e685c8dd4d708f342b3c0fee1aa190f92af39d
Shoeboxam <shoeboxam@gmail.com>
2017-10-21 13:20:04 -0500

    Insert entries into database example

---
+++ Scrapers/generic.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = client.aggregation
+
+db.records.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    })
+
+print(db.records.find_one({'borough': 'Manhattan'}))
