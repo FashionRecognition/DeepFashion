2b98d2e82daf1f29af4390679747d0095a44c86f
Shoeboxam <shoeboxam@gmail.com>
2017-10-21 16:23:53 -0500

    ML Tagger VGG16

---
+++ Tagger/openDataset.py
@@ -0,0 +1,9 @@
+import zipfile
+
+
+with open(r'C:\Users\mike_000\AppData\Local\Temp\list_attr_img.txt', 'r') as labels:
+    labels.read()
+    labels.read()
+
+    for label in labels:
+        print(label)
