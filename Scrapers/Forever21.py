38db373cda854b4f7fea862f04d4616831b6e839
Shoeboxam <shoeboxam@gmail.com>
2017-10-22 01:35:12 -0500

    Basic scraping

--- Scrapers/Forever21.py
+++ Scrapers/Forever21.py
@@ -8,8 +8,12 @@ from Scrapers.Listing import Listing
 
 from pymongo import MongoClient
 
+import logging
+
 client = MongoClient('localhost', 27017)
 db = client.aggregation
+
+logging.getLogger('scrapy').setLevel(logging.WARNING)
 
 
 # This is a partially converted skeleton for scraping Forever21.
@@ -19,7 +23,7 @@ class Forever21Spider(CrawlSpider):
     name = "forever21"
     store_url = "https://www.forever21.com"
     start_urls = [
-        store_url + "/us/shop/Catalog/Category/21men/mens-tops",
+        store_url + "/Catalog/Product/21men/mens-tops/2000216764",
     ]
 
     # This extracts a float number from a string
@@ -29,18 +33,7 @@ class Forever21Spider(CrawlSpider):
         'parse_product': '//div[@id="products"]',
         'parse_category_next': '//div[@id="h1Title"]/',
 
-        'parse_product_product_name': '//span[@itemprop="name"]/text()',
-        'parse_product_product_number_deal': '//div[@id="pd_deal"]//input[@name="pID"]/@value',
-        'parse_product_product_number_img': '//div[@id="pd_img"]//input[@name="pID"]/@value',
-        'parse_product_product_number_img_add2wl': '//span[@class="add2wl"]/@onclick',
-        'parse_product_product_number_share': '//div[@id="pd_deal"]//li[@class="email"]/a/@href',
-        'parse_product_description': '//div[@id="pd_desc"]//text()',
-        'parse_product_categories': '//div[@id="pd_bcr"]/ul//li/*/text()',
-        'parse_product_image_url': '//img[@id="pd_imgtag"]/@src',
-        'parse_product_availability': '//div[@id="pd_deal"]//p[@class="stock"]/strong/text()',
-        'parse_product_sale_price': '//span[@itemprop="price"]//text()',
-        'parse_product_on_sale_save': '//div[@class="midcart_widget"]//li[@class="save"]',
-        'parse_product_on_sale_img': '//div[@id="pd_img"]//div[contains(@class, "nl-promo")]',
+        'parse_product_product_name': 'id(\'h1Title\')/text()[1]',
 
     }
 
@@ -54,20 +47,19 @@ class Forever21Spider(CrawlSpider):
         xpath = self.xpaths[name_xpath]
         return hxs.select(xpath).extract()
 
-    def parse(self, response):
-        hxs = HtmlXPathSelector(response)
-        products = hxs.select(self.xpaths['parse_product']).extract()
-        print(products)
-        for product in products:
-            category_page = self.store_url + product
-            yield Request(category_page, callback=self.parse_product)
-        #
-        # next_page = hxs.select(self.xpaths['parse_category_next']).extract()
-        # if next_page:
-        #     next_page = self.store_url + str(next_page[0])
-        #     yield Request(next_page, callback=self.parse)
+    # def parse(self, response):
+    #     hxs = HtmlXPathSelector(response)
+    #     products = hxs.select(self.xpaths['parse_product']).extract()
+    #     for product in products:
+    #         category_page = self.store_url + product
+    #         yield Request(category_page, callback=self.parse_product)
+    #
+    #     next_page = hxs.select(self.xpaths['parse_category_next']).extract()
+    #     if next_page:
+    #         next_page = self.store_url + str(next_page[0])
+    #         yield Request(next_page, callback=self.parse)
 
-    def parse_product(self, response):
+    def parse(self, response):
         if response.url == self.store_url + '/':
             return
 
@@ -75,13 +67,14 @@ class Forever21Spider(CrawlSpider):
         item = Listing()
 
         # Source
-        item['source'] = self.store_url
+        item.source = self.store_url
 
         # Product Name
-        tmp = self.extract_xpath(hxs, 'parse_product_product_name')
-        if len(tmp) != 1:
-            raise ValueError('No Product Name')
-        item['product_name'] = tmp[0]
+        tmp = self.extract_xpath(hxs, 'parse_product_product_name')[0]
+        print(tmp)
+        # if len(tmp) != 1:
+        #     raise ValueError('No Product Name')
+        # item.name = tmp[0]
 
         # Actually insert to database
         db.records.insert_one(item.get_json())
--- Tagger/tagger.py
+++ Tagger/tagger.py
@@ -33,15 +33,15 @@ class Vgg16:
 
         # Convert RGB to BGR
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
-        assert red.get_shape().as_list()[1:] == [224, 224, 1]
-        assert green.get_shape().as_list()[1:] == [224, 224, 1]
-        assert blue.get_shape().as_list()[1:] == [224, 224, 1]
+        assert red.get_shape().as_list()[1:] == [200, 200, 1]
+        assert green.get_shape().as_list()[1:] == [200, 200, 1]
+        assert blue.get_shape().as_list()[1:] == [200, 200, 1]
         bgr = tf.concat(axis=3, values=[
             blue - VGG_MEAN[0],
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
         ])
-        assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
+        assert bgr.get_shape().as_list()[1:] == [200, 200, 3]
 
         self.conv1_1 = self.conv_layer(bgr, "conv1_1")
         self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
